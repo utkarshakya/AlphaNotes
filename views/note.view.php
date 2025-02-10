7e22977b78640faeae3a1bd54ef71b71881c341c
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-02-10 13:24:40 +0530

    Enhance CSS styles for layout and background colors; add new utility classes for height and width; update note view for improved textarea styling and layout

--- views/note.view.php
+++ views/note.view.php
@@ -3,12 +3,8 @@
 <?php require("partials/banner.php") ?>
 
 <main class="w-screen h-screen flex flex-col items-center">
-    <div class="w-full max-w-7xl px-5 py-10">
-        <div class="relative">
-            <label for="note"></label>
-            <textarea id="note" rows="10" class="w-full resize-none border-2 border-gray-500 rounded-md p-5"><?= $result['note'] ?></textarea>
-            <!-- <span id="showCharactersUsed" class="absolute bottom-3 right-3 text-gray-500">/200</span> -->
-        </div>
+    <div class="w-full max-w-7xl flex flex-col gap-5 px-5 py-10">
+        <textarea id="note" rows="10" class="w-full resize-none border-2 border-gray-500 rounded-md p-5 bg-gray-200/50"><?= $result['note'] ?></textarea>
         <button type="button" class="w-fit px-5 py-3 bg-indigo-500 hover:bg-indigo-600 cursor-pointer rounded-xl text-white font-semibold">Save Changes</button>
     </div>
 </main>
