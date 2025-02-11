44a46326fd64000b7b6986207518a33961ef6424
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-02-11 17:24:17 +0530

    Refactor functions for improved readability and maintainability; add abort function and use consistent formatting

--- Core/functions.php
+++ Core/functions.php
@@ -1,31 +1,41 @@
-<?php 
+<?php
 
-function dd($value = null){
+use Core\Response;
+
+function dd($value = null)
+{
     echo '<pre>';
-    $value === null ?  : var_dump($value);
+    $value === null ?: var_dump($value);
     echo '</pre>';
     die();
 };
 
-function isURL(string $value){
-   return($value == parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) ? true : false);
+function isURL(string $value)
+{
+    return ($value == parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) ? true : false);
 };
 
-function authorize($condition, $statusCode = Response::FORBIDDEN){
-    if(! $condition){
+function authorize($condition, $statusCode = Response::FORBIDDEN)
+{
+    if (! $condition) {
         abort($statusCode);
     }
 }
 
-function basePath($path){
-
-   return BASE_PATH . $path;
-    
+function basePath($path)
+{
+    return BASE_PATH . $path;
 }
 
-function view($path, $attributes = []){
-    
+function view($path, $attributes = [])
+{
     extract($attributes); // This will extract the values of a array.
-    
     require basePath("views/{$path}");
+}
+
+function abort($statusCode = 404)
+{
+    http_response_code($statusCode);
+    require basePath("controllers/$statusCode.php");
+    die();
 }
