60e20bae85bf083ad86288d1e88926e1bfb011e5
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-02-13 13:05:38 +0530

    Implement edit functionality for notes with dynamic button visibility and textarea enablement

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -5,3 +5,13 @@ textInput.addEventListener('input', () => {
     const count = textInput.value.length;
     charCount.textContent = `${count}/2500`
 })
+
+function editNote($textareaID, $clickedButtonID, $showButtonID){
+    const textarea = document.getElementById($textareaID);
+    const clickedButton = document.getElementById($clickedButtonID);
+    const showButton = document.getElementById($showButtonID);
+
+    textarea.removeAttribute("disabled");
+    clickedButton.setAttribute("hidden", "");
+    showButton.removeAttribute("hidden");
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -7,13 +7,14 @@
         <form action="/notes" method="post" class="flex flex-col gap-5">
             <label for="note"></label>
             <div class="relative">
-                <textarea id="note" name="note" rows="10" maxlength="2500" class="w-full border-2 focus:outline-none border-gray-500 md:text-xl p-10 bg-white"><?= htmlspecialchars($result['note']) ?></textarea>
+                <textarea required disabled id="note" name="note" rows="10" maxlength="2500" class="w-full disabled:bg-gray-50 disabled:cursor-no-drop border-2 focus:outline-none border-gray-500 md:text-xl p-10 bg-white"><?= htmlspecialchars($result['note']) ?></textarea>
                 <input type="hidden" name="_method" value="patch">
                 <input type="hidden" name="id" value="<?= $result['id'] ?>">
                 <span id="show-remaining-characters" class="absolute border rounded-br-xl top-0 left-0 px-2 py-1 bg-white text-xs text-gray-500 font-semibold"><?= strlen($result['note']) . "/2500" ?></span>
                 <p class="text-sm text-red-600 px-3 font-semibold"><?= !empty($error) ? $error["message"] : '' ?></p>
             </div>
-            <button type="submit" class="w-fit px-5 py-3 bg-indigo-500 hover:bg-indigo-600 rounded-md text-white font-semibold">Save Edits</button>
+            <button id="wantToEdit" type="button" class="w-fit px-5 py-3 bg-indigo-500 hover:bg-indigo-600 rounded-md text-white font-semibold" onclick="editNote('note', 'wantToEdit', 'saveUpdateChanges')">Edit</button>
+            <button hidden id="saveUpdateChanges" type="submit" class="w-fit px-5 py-3 bg-indigo-500 hover:bg-indigo-600 rounded-md text-white font-semibold">Save Changes</button>
         </form>
         <form action="/notes" method="post">
             <input type="hidden" name="_method" value="delete">
