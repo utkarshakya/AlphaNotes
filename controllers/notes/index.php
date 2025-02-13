a875fd725a1053f801c21bc4f1be61a6fd5b3409
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-02-13 12:23:54 +0530

    Update note retrieval query to order by last updated timestamp

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -5,7 +5,7 @@ use Core\Database;
 
 $db = App::resolve(Database::class);
 
-$query = "SELECT * FROM `notes` WHERE `user_id` = 1 ORDER BY `created_on` DESC";
+$query = "SELECT * FROM `notes` WHERE `user_id` = 1 ORDER BY `last_updated` DESC";
 $result = $db->query($query)->get();
 
 view("notes/index.view.php", ["heading" => "Notes", "result" => $result]);
