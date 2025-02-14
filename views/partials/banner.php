ec7f39d0c3ae9d96c61c0748512621ef1cae0384
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-02-14 10:02:30 +0530

    Add personalized greeting to banner and new width utility class

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,5 +1,10 @@
+<?php 
+$name = explode(' ', $_SESSION['username'])[0];
+$greeting = ["Hello", "Hola", "Namaste", "Konnichiwa", "Ni hao"];
+$index = rand(0,4);
+?>
 <header class="bg-white shadow-sm">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $heading ?></h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= "{$greeting[$index]}, {$name}" ?></h1>
     </div>
 </header>
